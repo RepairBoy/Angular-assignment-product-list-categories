652112a8a128e9ae4dc940d6e8f4a8c6f4b31be6
Vijin <vijinrobinson14@gmail.com>
2024-07-25 16:23:03 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardsComponent } from "./ui/cards/cards.component";
+import { Product} from './interface/appInterfaces';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myFirstAngularProject';
+  items:Product[] = [];
+  sectionCategory:string[] = [];
+  sectionTitle:string[] = [];
+  async ngOnInit(){
+   await this.fetchAPI()
+    if(this.items){
+      this.sectionCategory = this.getUniqueCategories(this.items);
+      this.sectionTitle = this.capitalizeFirstLetter(this.sectionCategory)
+      console.log(this.sectionCategory)
+    }
+  }
+
+  async fetchAPI(){
+    const url = 'https://dummyjson.com/products';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      this.items = data.products;
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  };
+  getUniqueCategories(items: Product[]): string[] {
+    const categories = items.map(item => item.category);
+    return Array.from(new Set(categories));
+  }
+  capitalizeFirstLetter(myString: string[]): string[] {
+    if (!myString) return myString;
+    let output:string[] = [];
+    myString.forEach(item =>{
+      output.push(item.charAt(0).toUpperCase() + item.slice(1))
+    })
+    return output;
+  }
 }
---
+++ src/app/interface/appInterfaces.ts
@@ -0,0 +1,8 @@
+
+export interface Product{
+    title:string;
+    thumbnail:string;
+    description:string;
+    id:number;
+    category:string;
+}
